d73a126b97e0f56239452f75d3b5ba5c9e29284b
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-06-02 16:55:44 -0700

    rsa simple example working

--- app/app/src/Chat.js
+++ app/app/src/Chat.js
@@ -35,6 +35,20 @@ export class Chat extends Component {
         }
     }
 
+    componentDidMount() {
+        document.title = "Chat - Secure Chat";
+
+        // RSA Example
+        const NodeRSA = require('node-rsa');
+        const key = new NodeRSA({b: 512});
+
+        const text = 'Hello RSA!';
+        const encrypted = key.encrypt(text, 'base64');
+        console.log('encrypted: ', encrypted);
+        const decrypted = key.decrypt(encrypted, 'utf8');
+        console.log('decrypted: ', decrypted);
+    }
+
 
     render() {
         if (this.props.authenticated === false)
@@ -55,10 +69,6 @@ export class Chat extends Component {
                 </Container>
             </Styles>
         )
-    }
-
-    componentDidMount() {
-        document.title = "Chat - Secure Chat";
     }
 
 }
