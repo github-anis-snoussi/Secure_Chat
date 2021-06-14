975cff9bbf73590bdce65a87c53dfa47783f7678
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-06-14 03:15:21 +0100

    claned code

--- CA/ca.py
+++ CA/ca.py
@@ -11,80 +11,66 @@ import uuid
 import subprocess
 
 
+# AUTH_key : authority key
+# to generate authority key : openssl genrsa -out AUTH_key -des3 4092
+
 
 # AUTH_cert : authority certif
-# AUTH_key : authority key
-
-class CA_admin:
-
-    @classmethod
-    def create_certificate(self, username):
-        # Generate our key
-        key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-        )
-        # Write our key to disk for safe keeping
-        with open('rsa_keys/'+username+"_key.pem", "wb") as f:
-            f.write(key.private_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PrivateFormat.TraditionalOpenSSL,
-                encryption_algorithm=serialization.BestAvailableEncryption(b"passphrase")
-                ))
-        # Generate a CSR
-        csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
-        # Provide various details about who we are.
-        x509.NameAttribute(NameOID.COUNTRY_NAME, u"TN"),
-        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"INSAT"),
-        x509.NameAttribute(NameOID.LOCALITY_NAME, u"INSAT"),
-        x509.NameAttribute(NameOID.ORGANIZATION_NAME, username),
-        x509.NameAttribute(NameOID.COMMON_NAME, username+'@insat.com'),
-        ])).add_extension(
-        x509.SubjectAlternativeName([
-                # Describe what sites we want this certificate for.
-                x509.DNSName(username+'@insat.com')
-                ]),
-        critical=False,
-        # Sign the CSR with our private key.
-        ).sign(key, hashes.SHA256(), default_backend())
-        # Write our CSR out to disk.
-        with open('csr/'+username+"_request.csr", "wb") as f:
-            f.write(csr.public_bytes(serialization.Encoding.PEM))
-        # Create the client certificate
-        pem_csr = open('csr/'+username+'_request.csr','rb').read()
-        csr = x509.load_pem_x509_csr(pem_csr, default_backend())
-        pem_cert = open('AUTH_cert', 'rb').read()
-        ca = x509.load_pem_x509_certificate(pem_cert, default_backend())
-        pem_key = open('AUTH_key','rb').read()
-        ca_key = serialization.load_pem_private_key(pem_key, password=bytes('rootroot', 'utf-8'), backend=default_backend())
-        builder = x509.CertificateBuilder()
-        builder = builder.subject_name(csr.subject)
-        builder = builder.issuer_name(ca.subject)
-        builder = builder.not_valid_before(datetime.datetime.now())
-        builder = builder.not_valid_after(datetime.datetime.now()+datetime.timedelta(7))
-        builder = builder.public_key(csr.public_key())
-        builder = builder.serial_number(int(uuid.uuid4()))
-        for ext in csr.extensions:
-            builder.add_extension(ext.value, ext.critical)
-        certificate = builder.sign(
-                private_key=ca_key,
-                algorithm=hashes.SHA256(),
-                backend=default_backend()
-                )
-        with open('certificates/'+username+'.crt', 'wb') as f:
-            f.write(certificate.public_bytes(serialization.Encoding.PEM))
-        return certificate.public_bytes(serialization.Encoding.PEM)
-
-    @classmethod
-    def verify_certificate(self, username, certif_string):
-        with open('tmp/'+username+'.crt', 'wb') as f:
-            f.write(certif_string)
-        output = subprocess.check_output('openssl verify -CAfile AUTH_cert tmp/'+username+'.crt', shell=True)
-        if (output[-3:-1]).decode('UTF-8') == 'OK':
-            return True
-        else:
-            return False
+# to generate authority certificate : openssl req -new -x509 -days 3650 -key AUTH_key -out AUTH_cert
 
 
-CA_admin.create_certificate("anis")
+def create_certificate(username):
+
+    # we simulate a key sent by the user
+    key = rsa.generate_private_key( public_exponent=65537,key_size=2048,backend=default_backend())
+
+    # Generate a Certificate Signing Request
+    csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
+    x509.NameAttribute(NameOID.COUNTRY_NAME, u"TN"),
+    x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"Centre Urbain Nord"),
+    x509.NameAttribute(NameOID.LOCALITY_NAME, u"INSAT"),
+    x509.NameAttribute(NameOID.ORGANIZATION_NAME, username),
+    x509.NameAttribute(NameOID.COMMON_NAME, username+'@insat.com'),
+    ])).add_extension(
+    x509.SubjectAlternativeName([x509.DNSName(username+'@insat.com')]),critical=False,
+    ).sign(key, hashes.SHA256(), default_backend()) # Sign the CSR with the user key
+
+    # Import the authority certificate and private key
+    pem_cert = open('AUTH_cert', 'rb').read()
+    pem_key = open('AUTH_key','rb').read()
+    ca = x509.load_pem_x509_certificate(pem_cert, default_backend())
+    ca_key = serialization.load_pem_private_key(pem_key, password=bytes('rootroot', 'utf-8'), backend=default_backend())
+    
+    # we build the the x509 certificate
+    builder = x509.CertificateBuilder()
+    builder = builder.subject_name(csr.subject)
+    builder = builder.issuer_name(ca.subject)
+    builder = builder.not_valid_before(datetime.datetime.now())
+    builder = builder.not_valid_after(datetime.datetime.now()+datetime.timedelta(7))
+    builder = builder.public_key(csr.public_key())
+    builder = builder.serial_number(int(uuid.uuid4()))
+    for ext in csr.extensions:
+        builder.add_extension(ext.value, ext.critical)
+
+    # we create the x509 certificate
+    certificate = builder.sign(private_key=ca_key,algorithm=hashes.SHA256(),backend=default_backend())
+
+    # we simulate saving the certificate in the LDAP server
+    with open('dummy_ldap/'+username+'.crt', 'wb') as f:
+        f.write(certificate.public_bytes(serialization.Encoding.PEM))
+
+    print('certificate generated and saved')
+
+
+def get_certificate(username):
+
+    # We simulate fetching the certificate from the LDAP server
+    fetched_certif = open('dummy_ldap/'+username+'.crt', 'rb').read()
+
+    # Here we return the certificate to the user
+    print(fetched_certif.decode())
+
+
+
+create_certificate("ee")
+# get_certificate("ansnoussi")
